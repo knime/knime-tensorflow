knime/knime-tensorflow

commit e1fb55829c71a1a3728c690883efc3c7dc6c9250

AP-8425: Replace assert with exception in TFModel

org.knime.dl.tensorflow/py/TFModel.py
@@ -93,7 +93,7 @@ class TFModel(object):
             self._sm_path = os.path.join(self._tmpdir.name, 'sm')
             self._save_saved_model(self._sm_path)
         else:
-            assert session is not None
+            raise ValueError('A session must be given if save is False')
 
     def save(self, path):
         if hasattr(self, '_sm_path'):

commit d87466905b0a9becd2800150b97f9cac19f2d680

AP-8425: Fix error introduced in e1fb558
In e1fb558 I replaced an assert with an ValueError but didn't check if
the assert was false before throwing the error.

org.knime.dl.tensorflow/py/TFModel.py
@@ -92,7 +92,7 @@ class TFModel(object):
             self._tmpdir = tempfile.TemporaryDirectory()
             self._sm_path = os.path.join(self._tmpdir.name, 'sm')
             self._save_saved_model(self._sm_path)
-        else:
+        elif session is None:
             raise ValueError('A session must be given if save is False')
 
     def save(self, path):